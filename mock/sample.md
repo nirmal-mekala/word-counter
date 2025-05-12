fb53d455ef222fd5ee8e4e1aef3b8e6bedba1247
Nirmal Mekala <ndmekala@icloud.com>
2025-05-12 14:28:01 -0500

    proof of concept in place - reading file from command line arg, watching file, converting to html, parsing html, logging with chalk

---
+++ mock/sample.md
@@ -0,0 +1,5 @@
+### here is a sample
+
+it has some paragraphs with<!--sneaky--> html comments within them
+
+it really does
--- src/index.js
+++ src/index.js
@@ -1,39 +1,82 @@
-const watch = require('watch');
+// const watch = require('watch');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const child_process = require('child_process');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const { hideBin } = require('yargs/helpers');
+const fs = require('fs');
+const readline = require('readline');
 
 const spawn = child_process.spawn;
 
-watch.watchTree(
-  '/Users/nirmal/ghq/github.com/nirmal-mekala/word-counter',
-  function (f, curr, prev) {
-    if (typeof f == 'object' && prev === null && curr === null) {
-      console.log('finished walking the tree');
-      // Finished walking the tree
-    } else if (prev === null) {
-      console.log('File ' + f + ' was created');
-      // f is a new file
-    } else if (curr.nlink === 0) {
-      console.log('File ' + f + ' was removed');
-      // f was removed
-    } else {
-      console.log('File ' + f + ' was changed');
-      // f was changed
-    }
-  },
-);
+const argv = yargs(hideBin(process.argv))
+  .options({
+    words: {
+      alias: 'w',
+      describe: '# additional words needed',
+      demandOption: true,
+    },
+    path: {
+      alias: 'p',
+      describe: 'provide a path to file',
+      demandOption: true,
+    },
+  })
+  .help()
+  .parse();
 
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-console.log(dom.window.document.querySelector('p').textContent); // "Hello world"
+console.log('argv', argv);
 
-pandoc = spawn('pandoc', ['--version']);
-pandoc.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+// TODO arg handling - should be able to set goal via command line
+// TODO break out
+// TODO error handlin
+// --> no file
+// --> no pandoc
+
+fs.watchFile(argv.path, (event, filename) => {
+  readline.cursorTo(process.stdout, 0, 0);
+  readline.clearScreenDown(process.stdout);
+  console.log(chalk.magenta('watchfileHook running'))
+  console.log(`argv.path ${argv.path} from watchFile hook`);
+  pandoc = spawn('pandoc', ['-s', argv.path]);
+  pandoc.stdout.on('data', (data) => {
+    //  console.log(`stdout: ${data}`);
+    const dom = new JSDOM(data);
+    console.log(
+      chalk.inverse.green(dom.window.document.querySelector('p').textContent),
+    );
+  });
+  pandoc.stderr.on('data', (data) => {
+    //    console.log(`stderr: ${data}`);
+  });
+  pandoc.on('close', (code) => {
+    //    console.log(`child process exited with code ${code}`);
+  });
 });
-pandoc.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-pandoc.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+
+// watch.watchTree(
+//   '/Users/nirmal/ghq/github.com/nirmal-mekala/word-counter',
+//   function (f, curr, prev) {
+//     if (typeof f == 'object' && prev === null && curr === null) {
+//       console.log('finished walking the tree');
+//       // Finished walking the tree
+//     } else if (prev === null) {
+//       console.log('File ' + f + ' was created');
+//       // f is a new file
+//     } else if (curr.nlink === 0) {
+//       console.log('File ' + f + ' was removed');
+//       // f was removed } else {
+//       console.log('File ' + f + ' was changed');
+//       // f was changed
+//     }
+//   },
+// );
+
+// const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+// console.log(dom.window.document.querySelector('p').textContent); // "Hello world"
+//
+//
+//
+
+// console.log(Array.from(document.querySelectorAll('li')).map(node => node.textContent).join("-----"))
